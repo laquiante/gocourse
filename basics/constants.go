9844f2d9db6cf1f276aae580ed76168db6067b40
alq <laquiante@aol.com>
2025-04-16 13:20:34 +0200

    Dienstag

---
+++ basics/constants.go
@@ -0,0 +1,17 @@
+package main
+
+const pi = 3.14
+const gravity = 9.81
+
+func main() {
+
+	const days int = 7
+
+	const (
+		monday        = 1
+		tuesday       = 2
+		wednesday     = 3
+		thursday  int = 4
+	)
+
+}
